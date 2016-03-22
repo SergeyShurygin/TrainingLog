774a16acbdf2b769d6db01264095d48fd29d0529
Sshur <sshur@mail.ru>
2016-03-22 12:09:17 +0300

    Added files via upload

---
+++ DashboardDesigner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrainingLog {
+    public partial class DashboardDesigner : Form {
+        public DashboardDesigner() {
+            InitializeComponent();
+        }
+
+        void DashboardDesigner_Load(object sender, EventArgs e) {
+            string fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "traininglog.xml");
+            dashboardDesigner1.LoadDashboard(fileName);
+        }
+    }
+}
